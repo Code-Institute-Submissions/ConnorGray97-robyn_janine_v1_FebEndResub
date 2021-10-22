df2c4a5ab8550d3c06cad6267bc3ba5fc7c95d81
ConnorGray97 <connor-gray@hotmail.com>
2021-10-22 19:06:24 +0000

    Updated script.js and seperated navs into includes

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 
 
-$(document).ready(function () {// Toggles the open and close full nav
+$(document).ready(function () { // Toggles the open and close full nav
     $('.toggle').click(function() {
        $(this).toggleClass('active');
        $('#fullnav').toggleClass('open');
@@ -8,7 +8,18 @@ $(document).ready(function () {// Toggles the open and close full nav
       });
 
 
-$(document).on('scroll', function(){  //Slides Bio into view
-    $('h2').css("left", Math.max(3000 - 1.5*window.scrollY,
-       100) + "px");
+
+// Slides h2 into hompage according to screen size
+
+$(document).on('scroll', function() {
+         if($(window).width() < 767)
+    {
+        $('h2').css("left", Math.max(2000 - 3*window.scrollY, 100) + "px");
+    } else if ($(window).width() > 767) {
+        $('h2').css("left", Math.max(2000 - 1*window.scrollY, 100) + "px");
+    }
+    
 })
+    
+
+
