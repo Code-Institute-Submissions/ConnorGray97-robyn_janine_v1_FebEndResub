c5ba2ea0ef7bf987776dc4ad23b7c4129016d0f9
ConnorGray97 <connor-gray@hotmail.com>
2021-10-25 15:03:10 +0000

    Deployment part 1 and added css and html to product page

--- robyn_janine/settings.py
+++ robyn_janine/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,11 +106,15 @@ WSGI_APPLICATION = 'robyn_janine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://fqzzuqzvmbqrbm:0253371a0d0640b47485edd0dbea97d61dbaa7708c202ebe825178bf35164608@ec2-52-211-158-144.eu-west-1.compute.amazonaws.com:5432/dcmah6ree04uv2')
 }
 
 
--- static/js/script.js
+++ static/js/script.js
@@ -12,14 +12,13 @@ $(document).ready(function () { // Toggles the open and close full nav
 // Slides h2 into hompage according to screen size
 
 $(document).on('scroll', function() {
-         if($(window).width() < 767)
+        if($(window).width() < 767)
     {
-        $('h2').css("left", Math.max(2000 - 3*window.scrollY, 100) + "px");
+        $('h2').css("left", Math.max(3000 - 1*window.scrollY, 100) + "px");
     } else if ($(window).width() > 767) {
-        $('h2').css("left", Math.max(2000 - 1*window.scrollY, 100) + "px");
+        $('h2').css("left", Math.max(3000 - 1*window.scrollY, 100) + "px");
     }
     
 })
     
-
 
