96c1e7c1c26504387f374c8352de17f6bb42cf64
ConnorGray97 <connor-gray@hotmail.com>
2021-10-30 11:51:20 +0000

    Category filter bug fix

--- products/views.py
+++ products/views.py
@@ -13,11 +13,10 @@ def all_products(request):
     categories = None
 
     if request.GET:
-
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
-            categories = category.objects.filter(name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -31,6 +30,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
