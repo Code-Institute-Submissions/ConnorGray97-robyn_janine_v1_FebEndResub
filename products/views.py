a83a5eccbc3c6a0d13e9062b2208f69945efee4a
ConnorGray97 <connor-gray@hotmail.com>
2022-02-07 15:55:24 +0000

    Added add to bag functionality

--- products/views.py
+++ products/views.py
@@ -42,7 +42,7 @@ def all_products(request):
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
+
     current_sorting = f'{sort}_{direction}'
 
     context = {
