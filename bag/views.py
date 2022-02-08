ebffdb4ad022ee27600f687bccde25cddbbafea6
ConnorGray97 <connor-gray@hotmail.com>
2022-02-08 16:03:10 +0000

    Updated Bag Views and URLs processor

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404)
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -12,14 +15,68 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+        if bag[item_id] + quantity <= 5:
+            bag[item_id] += quantity
+            messages.success(
+                request, f'Updated {product.name} quantity to {bag[item_id]}')
+        else:
+            messages.error(
+                request, "It's not possible to order more than 5")
     else:
-        bag[item_id] = quantity
+        if quantity <= 10:
+            bag[item_id] = quantity
+            messages.success(
+                request, f'Added {product.name} to your bag'
+            )
+        else:
+            messages.error(request, "There's a limit of 5!")
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        if bag[item_id] <= 5 and quantity <= 5:
+            bag[item_id] = quantity
+            messages.success(
+                request, f'Updated {product.name} quantity to {bag[item_id]}')
+    else:
+        bag.pop(item_id)
+        messages.success(
+            request, f'Removed {product.name} from your bag')
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Delete item from the shopping bag"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        bag = request.session.get('bag', {})
+
+        bag.pop(item_id)
+        messages.success(
+            request, f'Removed {product.name} from your bag')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(
+            request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
--- products/views.py
+++ products/views.py
@@ -61,7 +61,7 @@ def product_detail(request, product_id):
     products = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'product': product,
+        'product': products,
     }
 
     return render(request, 'products/product_detail.html', context)
