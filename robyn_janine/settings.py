26b206e6ad00178398827868dd6e1ba8bc9eea6d
ConnorGray97 <connor-gray@hotmail.com>
2022-02-10 23:00:29 +0000

    Setup crispy forms in settings

--- robyn_janine/settings.py
+++ robyn_janine/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     # other
 
     'storages',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'robyn_janine.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -86,6 +89,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
